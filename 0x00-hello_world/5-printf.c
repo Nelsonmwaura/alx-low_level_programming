8b773ad1b5fc9f5216c9de87836313545e60b1af
root <Nelsonmwaura>
2023-02-16 13:57:09 -0800

    use print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*  main - Entry point to print a String
+*  Description: using print f
+*  Return: 0
+*/
+int main(void)
+{
+printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
