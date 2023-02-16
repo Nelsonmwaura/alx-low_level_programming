3fe80a3a96a6dee8d21c0115d3e4a122d6746be9
root <Nelsonmwaura>
2023-02-16 14:38:11 -0800

    C quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,11 +11,14 @@ int main(void)
 	long fd = 1;
 	long syscall = 1;
 	long ret = 0;
-	__asm__ ("syscall"
+
+	__asm__ ("syscall"\n
+
 			: "=a" (ret)
 			: "a" (syscall),
 			"D" (fd),
 			"S" (s),
 			"d" (l));
+
 	return (1);
 }
